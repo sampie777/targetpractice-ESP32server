a9fa8fb5c2119b1280f7fac1b0c01d6a8b276cde
S. Jansen <sa.jansen1@gmail.com>
2022-01-26 17:48:39 +0200

    made settings configurable.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a7"
+#define CONFIG_CHARACTERISTIC_UUID "aeb5483e-46e1-5688-c7f5-ea07361b26a9"
 #define SENSOR_PIN 15
 #define LED_BUILTIN 2
 
@@ -23,14 +24,17 @@ int hitThreshold = 800;     // Out of 4095
 int hitForce = 0;
 unsigned long resetTime = millis();
 unsigned long hitDuration = 0;
+int brightness = 255;
 
 BLEServer *pServer;
-bool scanStarted = false;
+bool advertisingStarted = false;
 
 class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) override {
-        scanStarted = false;
+        advertisingStarted = false;
         std::string value = pCharacteristic->getValue();
+        Serial.print("default write: ");
+        Serial.println(value.c_str());
 
         if (value.length() <= 0) {
             return;
@@ -43,6 +47,7 @@ class MyCallbacks : public BLECharacteristicCallbacks {
             isHit = false;
             resetTime = millis();
             hitDuration = 0;
+            hitForce = 0;
             return;
         }
 
@@ -51,15 +56,10 @@ class MyCallbacks : public BLECharacteristicCallbacks {
             isOn = false;
             return;
         }
-
-        if (value.rfind("force;", 0) == 0){
-            sscanf(value.c_str(), "force;%d", &hitForce);
-            return;
-        }
     }
 
     void onRead(BLECharacteristic *pCharacteristic) override {
-        scanStarted = false;
+        advertisingStarted = false;
 
         if (isHit) {
             char *buffer;
@@ -70,6 +70,68 @@ class MyCallbacks : public BLECharacteristicCallbacks {
         } else {
             pCharacteristic->setValue("off");
         }
+    }
+};
+
+class ConfigCallback : public BLECharacteristicCallbacks {
+    typedef enum {
+        NONE,
+        FORCE,
+        BRIGHTNESS,
+    } ReadValue;
+    ReadValue requestedReadValue = ReadValue::NONE;
+
+    void onWrite(BLECharacteristic *pCharacteristic) override {
+        advertisingStarted = false;
+        std::string value = pCharacteristic->getValue();
+        Serial.print("config write: ");
+        Serial.println(value.c_str());
+
+        if (value.length() <= 0) {
+            return;
+        }
+
+        if (value.rfind("read;", 0) == 0 && value.length() < 5 + 8) {
+            char request[8];
+            sscanf(value.c_str(), "read;%s", request);
+            if (strcmp(request, "force") == 0) {
+                requestedReadValue = ReadValue::FORCE;
+            } else if (strcmp(request, "bright") == 0) {
+                requestedReadValue = ReadValue::BRIGHTNESS;
+            } else {
+                requestedReadValue = ReadValue::NONE;
+            }
+            return;
+        }
+
+        if (value.rfind("force;", 0) == 0) {
+            sscanf(value.c_str(), "force;%d", &hitThreshold);
+            return;
+        }
+
+        if (value.rfind("bright;") == 0) {
+            sscanf(value.c_str(), "bright;%d", &brightness);
+            return;
+        }
+    }
+
+    void onRead(BLECharacteristic *pCharacteristic) override {
+        advertisingStarted = false;
+
+        char *buffer;
+        switch (requestedReadValue) {
+            case ReadValue::FORCE:
+                asprintf(&buffer, "force;%d", hitThreshold);
+                break;
+            case ReadValue::BRIGHTNESS:
+                asprintf(&buffer, "bright;%d", brightness);
+                break;
+            default:
+                pCharacteristic->setValue("");
+                return;
+        }
+
+        pCharacteristic->setValue(buffer);
     }
 };
 
@@ -158,12 +220,21 @@ void setup() {
     );
     pCharacteristic->setCallbacks(new MyCallbacks());
     pCharacteristic->setValue("");
+
+    BLECharacteristic *configCharacteristic = pService->createCharacteristic(
+            CONFIG_CHARACTERISTIC_UUID,
+            BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE
+    );
+    configCharacteristic->setCallbacks(new ConfigCallback());
+    configCharacteristic->setValue("");
+
     pService->start();
 
     // Start advertising
     BLEAdvertising *pAdvertising = pServer->getAdvertising();
     pAdvertising->start();
-    scanStarted = true;
+    advertisingStarted = true;
 
     Serial.println("Ready.");
 }
@@ -171,9 +242,9 @@ void setup() {
 void loop() {
     checkIfIsHit();
     outputIsHit();
-    if (!scanStarted && pServer->getConnectedCount() == 0) {
+    if (!advertisingStarted && pServer->getConnectedCount() == 0) {
         pServer->startAdvertising();
-        scanStarted = true;
+        advertisingStarted = true;
     }
     delay(1);
 }
